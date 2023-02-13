374899ec5389cd14f9d4d7774d75f2bdb0ccce58
Jonny Lin <jonny@snippyly.com>
2023-02-13 14:36:55 -0800

    Added state

--- src/components/DemoGrid.tsx
+++ src/components/DemoGrid.tsx
@@ -1,19 +1,90 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSnippylyClient } from '@snippyly/react';
 import styles from './DemoGrid.module.css';
 // @ts-ignore
-import { Grid } from 'react-spreadsheet-grid';
-import { DemoColumns, DemoRows } from "../util/data";
-
-
+import { Grid, Input } from 'react-spreadsheet-grid';
+import { DemoRows } from "../util/data";
+import './spreadsheet_override.css';
 
 const DemoGrid = () => {
 
   const { client } = useSnippylyClient();
+  const [rows, setRows] = useState(DemoRows);
+
+  const onFieldChange = (rowId: number, field: string) => (value: string) => {
+    const row = rows.find(({ id }) => id === rowId);
+    row[field] = value;
+    setRows([].concat(...rows));
+  }
+
+  const initColumns = () => {
+    return [
+      {
+        title: () => "Quarter",
+        value: (row: any, { focus }: any) => {
+          return (
+            <Input
+              value={row.quarter}
+              focus={focus}
+              onChange={onFieldChange(row.id, 'quarter')}
+            />
+          )
+        }
+      },
+      {
+        title: () => "Revenue",
+        value: (row: any, { focus }: any) => {
+          return (
+            <Input
+              value={row.revenue}
+              focus={focus}
+              onChange={onFieldChange(row.id, 'revenue')}
+            />
+          )
+        }
+      },
+      {
+        title: () => "Operating Cost",
+        value: (row: any, { focus }: any) => {
+          return (
+            <Input
+              value={row.operatingCost}
+              focus={focus}
+              onChange={onFieldChange(row.id, 'operatingCost')}
+            />
+          )
+        }
+      },
+      {
+        title: () => "Net Income",
+        value: (row: any, { focus }: any) => {
+          return (
+            <Input
+              value={row.netIncome}
+              focus={focus}
+              onChange={onFieldChange(row.id, 'netIncome')}
+            />
+          )
+        }
+      },
+      {
+        title: () => "Balance Sheet",
+        value: (row: any, { focus }: any) => {
+          return (
+            <Input
+              value={row.balanceSheet}
+              focus={focus}
+              onChange={onFieldChange(row.id, 'balanceSheet')}
+            />
+          )
+        }
+      },
+    ];
+  };
 
   return (
     <Grid
-      columns={DemoColumns}
+      columns={initColumns()}
       rows={DemoRows}
       getRowKey={(row: any) => row.id}
     >
